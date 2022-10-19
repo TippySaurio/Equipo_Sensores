83fb1c42a2d7ad5a7254fd53a862b8a8292469ca
karla Chávez <79483512+karlachavezc@users.noreply.github.com>
2022-10-19 14:47:45 -0700

    Create KY-003.py

---
+++ KY-003.py
@@ -0,0 +1,13 @@
+from machine import Pin  
+import time,utime
+ 
+he=Pin(26,Pin.IN)  
+led=Pin(11,Pin.OUT)  
+while True:  
+   try:  
+     if he.value() == 0:  
+       led.value(0)  
+     elif he.value()==1:  
+       led.value(1)  
+   except KeyboardInterrupt:  
+     break 
