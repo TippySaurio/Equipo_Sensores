04dd76fb6962537c08ad709e44696e0b8ff88770
Jesus Arturo Bustamante Garces <60672400+Busta50@users.noreply.github.com>
2022-10-25 14:34:28 -0700

    Create main.py

---
+++ Ky-36/main.py
@@ -0,0 +1,16 @@
+# KY - 036 J.BUSTAMANTE
+
+from machine import Pin
+import utime
+
+flame_sensor = Pin(16, Pin.IN)
+utime.sleep(2)
+
+while True:
+   if flame_sensor.value() == 1:
+       print("Tocando")
+       utime.sleep(3)
+   else:
+       print("Libre")
+       utime.sleep(1)
+utime.sleep(0.1)
