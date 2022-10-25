58adc42fa342e677cf35e7d7c34f4db1dba56c94
Jesus Arturo Bustamante Garces <60672400+Busta50@users.noreply.github.com>
2022-10-25 14:39:33 -0700

    Create main.py

---
+++ Ky-028/main.py
@@ -0,0 +1,13 @@
+# KY - 028 J.BUSTAMANTE
+
+from machine import Pin
+import utime
+ 
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+ 
+while True:
+    reading = sensor_temp.read_u16() * conversion_factor 
+    temperature = 27 - (reading - 0.706)/0.001721
+    print(temperature)
+    utime.sleep(2)
