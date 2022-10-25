f086ee05c99036138fd9d237d38fa3981aeb104d
Jesus Arturo Bustamante Garces <60672400+Busta50@users.noreply.github.com>
2022-10-25 14:15:42 -0700

    Update main.py

--- Ky-026/main.py
+++ Ky-026/main.py
@@ -1,38 +1,16 @@
-from machine import Pin, PWM
+#KY - 026 J. BUSTAMANTE
+
+from machine import Pin
 import utime
 
 flame_sensor = Pin(16, Pin.IN)
-buzzer = Pin(17, Pin.OUT)
-utime.sleep(0.5)
-
-buzzer.high()
-
-#OUT1 y OUT2
-In1=Pin(1,Pin.OUT)  #IN1`
-In2=Pin(0,Pin.OUT)  #IN2
-EN_A=PWM(Pin(2))
-
-# Definición de frecuencia para habilitar pines
-EN_A.freq(1500)
-
-duty_cycle = 65535
+utime.sleep(2)
 
 while True:
-    while flame_sensor.value() == 1:
-        print("Flame Detected")
-        
-        buzzer.low()
-        In1.low()
-        In2.high()
-        EN_A.duty_u16(int(duty_cycle/2))
-    
-    if flame_sensor.value() == 0:
-                
-        buzzer.high()
-        In1.low()
-        In2.low()
-        print("No Flame")
-
-
-
-utime.sleep(0.2)
+   if flame_sensor.value() == 1:
+       print("Detección de flama")
+       utime.sleep(3)
+   else:
+       print("No Flama")
+       utime.sleep(1)
+utime.sleep(0.1)
