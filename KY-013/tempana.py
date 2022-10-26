0237e9dcf747de4ccfa52cd40a69c4ec7b1ad8c6
karla Chávez <79483512+karlachavezc@users.noreply.github.com>
2022-10-25 19:46:43 -0700

    Create tempana.py

---
+++ KY-013/tempana.py
@@ -0,0 +1,24 @@
+#karla chavez carballo
+#17210542
+#SENSOR DE TEMPERATURA ANALOGO
+
+import machine
+from utime import sleep
+from math import log
+
+def main():
+    sensor_temperatura = machine.ADC(26)
+    c1 = 0.001129148
+    c2 = 0.000234125
+    c3 = 0.0000000876741
+    R1 = 10000.0
+
+    while True:
+        R2 = R1 / (65535 / (float(sensor_temperatura.read_u16()) - 1))
+        logR2 = log(R2)
+        temperatura = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2))-281.10
+        print('Temperatura: ' + str(temperatura) + ' °C')
+        sleep(2)
+
+if __name__ == '__main__':
+    main()
