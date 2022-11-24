e42628153c1b0858ad671369b328ec25449f8c46
TippySaurio <106705252+TippySaurio@users.noreply.github.com>
2022-11-23 20:27:53 -0800

    Add files via upload

---
+++ Ky-019.py
@@ -0,0 +1,19 @@
+# Diaz Rodriguez Jorge Said - 19211629
+# 
+# Micropython
+
+from machine import Pin
+import time
+
+led_pins = [22,26,27]
+leds = [Pin(led_pins[0],Pin.OUT),Pin(led_pins[1],Pin.OUT),
+        Pin(led_pins[2],Pin.OUT)]
+delay_t = 0.1
+while True:
+    leds[0].high()
+    leds[1].high()
+    leds[2].high()
+    time.sleep(1)
+    leds[2].low()
+    leds[0].high()
+    leds[1].high()
